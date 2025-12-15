2c7aa0bec7dff62a8d296e64b3ecec83cccbd7d6
Jordy de Vulder <jordy@justbetter.nl>
2025-12-15 11:59:10 +0100

    Added extra button fieldset options

--- resources/views/components/fieldset/link.blade.php
+++ resources/views/components/fieldset/link.blade.php
@@ -4,9 +4,10 @@
     <x-rapidez::tag
         :is="!$disable && $link['link']->value() ? 'a' : 'div'"
         :href="!$disable && $link['link']->value() ? $link['link']->value() : null"
-        :rel="!$disable && $link['link']->value() && $link['link_options'] ? $link->link_options['rel']->value() : null"
+        :rel="!$disable && $link['link']->value() && $link['link_options'] && $link->link_options['rel'] ? $link->link_options['rel']->value() : null"
         :target="!$disable && $link['link']->value() && $link['link_options'] && $link->link_options['blank'] ? '_blank' : null"
-        :aria-label="!$disable && $link['link']->value() && $link['link_options'] ? $link->link_options['aria_label'] : null"
+        :aria-label="!$disable && $link['link']->value() && $link['link_options'] ? $link->link_options['aria_label'] : null" 
+        :download="!$disable && $link['link']->value() && $link['link_options'] && $link->link_options['download_attribute'] ? '' : false"
         {{ $attributes }}
     >
         {{ $slot }}
