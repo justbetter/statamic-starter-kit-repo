da522e4d02483988ccb264be32037d9e12b3d2d6
Kevin Meijer <kevin@justbetter.nl>
2026-02-26 13:44:46 +0100

    Added Image Optimize addon

---
+++ config/image-optimize.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    // Set the default resize width in pixels
+    'default_resize_width' => env('IMAGE_OPTIMIZE_WIDTH', 2560),
+
+    // Set the default resize height in pixels
+    'default_resize_height' => env('IMAGE_OPTIMIZE_HEIGHT', 2560),
+
+    // Set the default queue name
+    'default_queue_name' => env('IMAGE_OPTIMIZE_QUEUE_NAME', 'default'),
+
+    // Set the default queue connection
+    'default_queue_connection' => env('IMAGE_OPTIMIZE_QUEUE_CONNECTION', env('QUEUE_CONNECTION', 'sync')),
+
+    // The following mime types will be used to optimize images
+    'mime_types' => ['image/jpeg', 'image/png', 'image/gif', 'image/webp'],
+
+    // You can exclude containers from optimization entirely here
+    'excluded_containers' => ['icons'],
+];
