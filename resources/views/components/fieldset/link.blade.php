e81cfba0eccfdc6f85dccfbb16efce7160c7abee
Roene <roene@justbetter.nl>
2026-04-16 11:26:00 +0200

    Added comments how to use fieldsets | Updated Readme with fieldset examples

--- resources/views/components/fieldset/link.blade.php
+++ resources/views/components/fieldset/link.blade.php
@@ -27,7 +27,7 @@ child element that shouldn't be an anchor tag.
         :href="!$disable && $link['link']->url() ? $link['link']->url() : null"
         :rel="!$disable && $link['link']->url() && $link['link_options'] && $link->link_options['rel'] ? collect($link->link_options['rel'])->pluck('value')->implode(' ') : null"
         :target="!$disable && $link['link']->url() && $link['link_options'] && $link->link_options['blank'] ? '_blank' : null"
-        :aria-label="!$disable && $link['link']->url() && $link['link_options'] ? $link->link_options['aria_label'] : null" 
+        :aria-label="!$disable && $link['link']->url() && $link['link_options'] ? $link->link_options['aria_label'] : null"
         :download="!$disable && $link['link']->url() && $link['link_options'] && $link->link_options['download_attribute'] ? '' : false"
         {{ $attributes }}
     >
