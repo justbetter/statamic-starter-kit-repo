1a587abf129efc8fbb5f71d0b5724fc455bc8041
Roene <roene@justbetter.nl>
2026-04-16 11:33:22 +0200

    Remove blade file

--- resources/views/page_builder/component_test.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <x-fieldset.title :title="$title?->value()" class="text-primary"/>
-    <x-fieldset.button :button="$button->value()" class="my-4"/>
-    <x-fieldset.buttons :buttons="$buttons?->value()" />
-    <x-fieldset.buttons :buttons="$buttons?->value()" class="mt-10">
-        <x-slot:button class="inline-flex"></x-slot:button>
-    </x-fieldset.buttons>
-    <x-fieldset.icon :icon="$icon?->value()" class="h-20 text-red-500"/>
-</div>
