f6e44b4cecf217b5bd9565f2121ed505fd8936b3
Kevin Meijer <kevin@justbetter.nl>
2026-04-21 16:10:58 +0200

    Remove svg dimension validator

--- package/src/ServiceProvider.php
+++ package/src/ServiceProvider.php
@@ -4,7 +4,6 @@ namespace JustBetter\StatamicStarterKit;
 
 use Illuminate\Routing\Router;
 use JustBetter\StatamicStarterKit\Http\Controllers\CP\StarterKitFormsController;
-use JustBetter\StatamicStarterKit\Validation\SvgDimensionsValidation;
 use Statamic\Facades\Icon;
 use Statamic\Http\Controllers\CP\Forms\FormsController;
 use Statamic\Http\Middleware\RedirectAbsoluteDomains;
@@ -20,7 +19,6 @@ class ServiceProvider extends AddonServiceProvider
         });
 
         $this->app->singleton(FormsController::class, StarterKitFormsController::class);
-        SvgDimensionsValidation::register();
 
         Icon::register('custom-svg', resource_path('svg'));
         Icon::register('custom-icons', public_path('icons'));
