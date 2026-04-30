9aa58588104438616834a0b7a26ac1756cfd3cda
Kevin Meijer <kevin@justbetter.nl>
2026-04-30 16:21:52 +0200

    Removed unused translations

--- lang/en/validation.php
+++ lang/en/validation.php
@@ -4,6 +4,5 @@ return [
 
     'unique_entry_value' => 'The :attribute has already been taken.',
     'unique_user_value' => 'The :attribute has already been taken.',
-    'svg_has_dimensions' => 'The :attribute must contain width and height attributes or a valid viewBox on the root svg element.',
 
 ];
--- lang/nl/validation.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    'svg_has_dimensions' => 'Het veld :attribute moet width- en height-attributen of een geldige viewBox in het root svg element bevatten.',
-];
